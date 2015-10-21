58704c32dfda516128b02fcec90ddbe190964c7d
caieva21 <caieva21@gmail.com>
2015-10-21 08:34:55 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,11 @@
-class NamedCow
+NamedCow extends Cow 
 {
-
+  private String myName;
+  public Cow(String type, String name, String sound) 
+    {
+      myType = type;
+      mySound = sound;
+      myName = name;
+    }
+   public String getName() {return myName;}
 }
